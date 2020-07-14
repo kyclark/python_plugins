bcec1f9d8d1fdca21b86724def54db1b0678f3ff
Ken Youens-Clark <kyclark@gmail.com>
2020-07-14 15:51:46 -0700

    foo

---
+++ test_driver.py
@@ -0,0 +1,58 @@
+import os
+import random
+import re
+import string
+from subprocess import getstatusoutput
+
+prg = './driver.py'
+
+
+# --------------------------------------------------
+def test_exists() -> None:
+    """Program exists"""
+
+    assert os.path.isfile(prg)
+
+
+# --------------------------------------------------
+def test_invalid_language() -> None:
+    """Rejects invalid language"""
+
+    lang = random_string()
+    name = random_string()
+    rv, out = getstatusoutput(f'{prg} {lang} {name}')
+    assert rv != 0
+    assert re.search(f"argument lang: invalid choice: '{lang}'", out)
+
+
+# --------------------------------------------------
+def test_english() -> None:
+    """Test greet in English"""
+
+    name = random_string()
+    run('english', name, f'Hello, {name}!')
+
+
+# --------------------------------------------------
+def test_spanish() -> None:
+    """Test greet in Spanish"""
+
+    name = random_string()
+    run('spanish', name, f'¡Hola, {name}!')
+
+
+# --------------------------------------------------
+def random_string():
+    """generate a random string"""
+
+    k = random.randint(5, 10)
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
+
+
+# --------------------------------------------------
+def run(lang: str, name: str, expected: str) -> None:
+    """Run program with options"""
+
+    rv, out = getstatusoutput(f'{prg} {lang} {name}')
+    assert rv == 0
+    assert out == expected
